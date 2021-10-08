a78be94529411ce78d502b662bbedb93982e8768
mr687 <davinomoehdanino@gmail.com>
2021-10-08 17:46:29 +0700

    :sparkles: add search view

--- Assignment6H8_DaviNomoehDani/resources/views/users/index.blade.php
+++ Assignment6H8_DaviNomoehDani/resources/views/users/index.blade.php
@@ -5,8 +5,16 @@
 
   @include('users/partials/flash_msg')
 
-  <div class="text-end mb-3">
-    <a href="{{ route('users.create') }}" class="btn btn-success">Create New User</a>
+  <div class="d-flex justify-content-between my-3">
+    <div>
+      <form action="{{ route('users.index') }}" class="d-flex" method="GET">
+        <input type="text" placeholder="Search by name" class="form-control" value="{{ request()->query->get('query') }}" name="query">
+        <button type="submit" class="btn btn-primary">Search</button>
+      </form>
+    </div>
+    <div>
+      <a href="{{ route('users.create') }}" class="btn btn-success">Create New User</a>
+    </div>
   </div>
   <table class="table table-bordered table-striped">
     <thead>
